94d5c5d271fd74a943c612135e5f68f2fff4d2f0
Nakanishi Toshimasa <tabibito1207@gmail.com>
2021-01-18 11:42:16 +0900

    no message

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// Require Plugins
+// ---------------------------------------------------------------------------
+// var browserSync = require("browser-sync").create();
+// var bulkSass = require('gulp-sass-bulk-import');
+// var changed  = require('gulp-changed');
+// var cleanCSS = require('gulp-clean-css');
+// var concat = require('gulp-concat');
+// var gulp = require('gulp');
+// var pug = require('gulp-pug');
+// var htmlmin = require('gulp-htmlmin');
+// var imagemin  = require ('gulp-imagemin');
+// var mozjpeg = require('imagemin-mozjpeg');
+// var newer = require('gulp-newer');
+// var notify  = require('gulp-notify');
+// var plumber = require('gulp-plumber');
+// var pngquant = require('imagemin-pngquant');
+// var rename = require('gulp-rename');
+// var rimraf = require('rimraf');
+var sass = require('gulp-sass');
+// var sourcemaps = require('gulp-sourcemaps');
+// var uglify = require('gulp-uglify');
+// var watch = require("gulp-watch");
